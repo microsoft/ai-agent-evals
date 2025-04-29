c6f230d92444ea936163f6f107ea06b9d65218a1
Ritesh Kumar Sinha <risinh@microsoft.com>
2025-04-29 15:22:15 -0700

    Fixing lint issues

--- tasks/AIAgentReport/eslint.config.js
+++ tasks/AIAgentReport/eslint.config.js
@@ -1,28 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        React: 'readonly' // helps with JSX rules
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...react.configs.recommended.rules, // load react plugin rules
       ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // allow JSX without import React
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
+    settings: {
+      react: {
+        version: 'detect', // auto-detect React version
+      },
+    },
   },
-)
+);
