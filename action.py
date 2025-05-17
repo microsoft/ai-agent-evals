67da30143941ec8fe35dad7b053de4595106710e
Ritesh Kumar Sinha <risinh@microsoft.com>
2025-05-17 13:28:00 -0700

    Fix lint error

--- action.py
+++ action.py
@@ -341,11 +341,10 @@ def main(
           used for comparison.
     """
     working_dir = Path(".") if working_dir is None else working_dir
-    credential = DefaultAzureCredential()
     project_client = AIProjectClient(
         credential=credential,
         endpoint=endpoint,
-        api_version = "2025-05-15-preview" # Evaluations yet not supported on stable (api_version="2025-05-01")
+        api_version="2025-05-15-preview", # Evaluations yet not supported on stable (api_version="2025-05-01")
     )
 
     parsed_url = urlparse(endpoint)
